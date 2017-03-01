956db32e975b80dd218c054f92285db40aab89fb
Bernhard Geßler <Bernhard Geler>
2017-03-01 14:00:29 +0700

    Added support for new Bookings API. Added examples for Bookings and Content API.

---
+++ examples/exampleBookings.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once __DIR__ . '\..\src\tglApiClient.php';
+
+
+// Instantiate API client and sign in
+$key ="e193c470-cc9a-48f7-ab89-4d2429066864";
+$client = new TglApiClient();
+$client->signInWithApiKey($key);
+
+
+//date_default_timezone_set('UTC');
+$newBooking = array(
+        'reference' => "K0038", // Optional. Your internal customer number.
+        //'dateArrival' => date("Y-m-d\TH:i:s\Z", mktime(0, 0, 0, 3, 26, 2017)), // Optional
+        'comment' => "API Test", // Optional
+        'dateStart' => date("Y-m-d\TH:i:s\Z", mktime(0, 0, 0, 3, 27, 2017)),
+        'fees' => array( // Optional
+            array(
+                'quantity' => 5,
+                'sku' => "upgrade-comfort"
+            )
+        ),
+        //'flightNumber' => "DE123", // Optional,
+        'participant' => array(
+            birthday => date("Y-m-d\TH:i:s\Z", mktime(0, 0, 0, 3, 11, 2002)),
+            countryCode => 'DE', //Nationality as ISO 3166-1 alpha-2 code
+            email => 'test@gmail.com', // Optional
+            firstName => 'Claus',
+            gender => 0, // 0=Male, 1=Female
+            lastName => 'Santa',
+            passportNumber => 'XK2F11LA5' // Optional
+        ),
+        'programs' => array(
+            array( 'id' => '1mKY3DddPftfvIDPK959drDwyOnTIuLiHUf3gWqdhZ9A' ),
+            array( 'id' => '1bkBXHXgtsqCkSsi73SaIbT2CTzo-kPN7mazp4sT9o0k' ),
+            array( 'id' => '1bkBXHXgtsqCkSsi73SaIbT2CTzo-kPN7mazp4sT9o0k' ),
+            array( 'id' => '1bkBXHXgtsqCkSsi73SaIbT2CTzo-kPN7mazp4sT9o0k' ),
+            array( 'id' => '1bkBXHXgtsqCkSsi73SaIbT2CTzo-kPN7mazp4sT9o0k' )
+        )
+    
+);
+
+// Make a new booking. We'll get back the booking ID'
+$bookingId = $client->createBooking($newBooking);
+
+// Retrieve that booking again from the API
+$booking = $client->getBooking($bookingId);
+
+// Now the participant confirmed his arrival details. Update booking.
+$booking->dateArrival = date("Y-m-d\TH:i:s\Z", mktime(10, 5, 0, 3, 26, 2017));
+$booking->flightNumber = "DE123";
+$client->updateBooking($bookingId, $booking);
+
+// Participant canceled his trip.
+// We will later still be able to get this booking via the API but 'isCanceled' will be 'true'
+$client->cancelBooking($bookingId);
+
+
+
+echo $booking;
+?>
---
+++ examples/exampleContent.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '\..\src\tglApiClient.php';
+
+
+// Instantiate API client and sign in
+$key ="e193c470-cc9a-48f7-ab89-4d2429066864";
+$client = new TglApiClient();
+$client->signInWithApiKey($key);
+
+// Get the Thailand Umphang location document
+$document = $client->getDocument("1qHoygXnd8S3JSzFqteOtSXXM-LMTdDGS6658nKeRwqQ");
+
+// Get the price for upgrading upgrading the room to $name
+$name = $document->accommodation->types[1]->name;
+$price = $document->accommodation->types[1]->upgradePrice->CurrentPrice;
+
+// Suppose our retail price is 45USD per point
+echo "Upgrade to " . $name . " costs " . ($price * 45) . "USD per week.";
+?>
